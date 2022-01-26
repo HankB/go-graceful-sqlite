9a19205ac07c2949ede9f750515208e10e7a1b37
Hank Barta <hbarta@gmail.com>
2022-01-25 21:24:17 -0600

    first working code

---
+++ go-sqlite.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	initDataBase("sqlite3", dbName)
+}
+
+var dbName = "test.db"
+
+//var dbName = ":memory:"
+var db *sql.DB
+var err error
+
+/* init the database or go up in flames
+ */
+func initDataBase(flavor, location string) {
+	if location != ":memory:" {
+		fmt.Println("Removing", location)
+		os.Remove(location) // remove previous copy
+	}
+
+	// open the database
+	db, err = sql.Open(flavor, location)
+	if err != nil {
+		fmt.Println("error opening", location)
+		log.Fatal(err)
+	}
+	fmt.Println("opened", location)
+
+	sqlCreateStmt := `create table data (value integer not null);`
+	// create the table
+	_, err = db.Exec(sqlCreateStmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("created table \"data\"")
+}
